c4dbb43687f41c84f812c2cb2aa56ff9c82f2002
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2021-11-17 14:43:04 -0300

    modif. estilo

--- Vista/estructura/header.php
+++ Vista/estructura/header.php
@@ -4,18 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../css/fontawesome/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Architects+Daughter">
     <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="../../vendor/almasaeed2010/adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <link rel="stylesheet" href="../css/toastr.css">
+    <link rel="stylesheet" href="../css/toastr.css"><link rel="stylesheet" href="../css/style.css">
     <script src="../js/jquery/jquery-3.6.0.min.js"></script>
     <title><?php $Titulo ?></title>
   </head>
   <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
+    <div class="wrapper" id="wrapM">
       <!-- Navbar -->
-      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light" id="header-nav">
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
@@ -27,10 +28,10 @@
       </nav>
       <!-- /Navbar -->
       <!-- side menu -->
-      <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
-        <a href="#" class="brand-link">
-          <img src="#" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">Label Logo</span>
+      <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4" id='asidecont'>
+        <a href="../public/Index.php" class="brand-link">
+          <img src="../css/img/log3.png" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span id='logot' class="brand-text font-weight-light">Toc Toc</span>
         </a>
         <div class="sidebar">
           <!--menu-->
@@ -50,7 +51,7 @@
         if($objSess->activa()){
           echo '
             <div class="sidebar-custom">
-              <a href="../accion/cerrarSesion.php" class="btn btn-danger hide-on-collapse pos-right">Logout</a>
+            <a href="../accion/cerrarSesion.php" class="link-danger hide-on-collapse pos-right m-0" id="cerrarSess" >Cerrar sesion</a>
             </div>';
         }
         ?>
--- Vista/public/contacto.php
+++ Vista/public/contacto.php
@@ -11,12 +11,12 @@ include_once("../estructura/header.php");
 ?>
 <div class="content-wrapper">
     <div class="card">
-        <div class="card-body row">
-            <div class="col-5 text-center d-flex align-items-center justify-content-center">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.3626106990037!2d-68.05820688495247!3d-38.96138740834625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33dd78ff8aa5%3A0x62644f9a06d82181!2sToc%20Toc!5e0!3m2!1ses!2sar!4v1636487946382!5m2!1ses!2sar" width="700" height="500" style="border:2px goldenrod solid;" allowfullscreen="" loading="lazy"></iframe>
+        <div class="card-body row mt-4">
+            <div class="col-5 text-center d-flex align-items-center justify-content-center mx-4 mb-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.3626106990037!2d-68.05820688495247!3d-38.96138740834625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33dd78ff8aa5%3A0x62644f9a06d82181!2sToc%20Toc!5e0!3m2!1ses!2sar!4v1636487946382!5m2!1ses!2sar" width="700" height="500" style="border:2px goldenrod solid;border-radius:3px" allowfullscreen="" loading="lazy"></iframe>
             </div>
-            <div class="col-7">
-                <h3>Contactanos!</h3>
+            <div class="col-6 mx-4">
+                <h3 id='Contacto' class="textArch">Contactanos!</h3>
                 <div class="form-group">
                     <label for="inputName">Name</label>
                     <input type="text" id="inputName" class="form-control">
@@ -34,7 +34,7 @@ include_once("../estructura/header.php");
                     <textarea id="inputMessage" class="form-control" rows="4"></textarea>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Send message">
+                    <input type="submit" class="btn btn-outline-primary" value="Enviar">
                 </div>
             </div>
         </div>
