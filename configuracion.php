b53644d12ce12284654ab4be360ebc7c82877950
Raam4 <ramiro_cardozo22@hotmail.com>
2021-11-17 21:22:30 -0300

    home

--- configuracion.php
+++ configuracion.php
@@ -5,7 +5,7 @@
 // CONFIGURACION APP//
 /////////////////////////////
 
-$PROYECTO ='PWD_TPFinal---prueba';
+$PROYECTO ='PWD_TPFinal';
 
 //variable que almacena el directorio del proyecto
 $ROOT =$_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
