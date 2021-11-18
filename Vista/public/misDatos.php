5b0dff3d69c9f477f1d6a001fb9c42f86dc9a006
Raam4 <ramiro_cardozo22@hotmail.com>
2021-11-18 14:35:44 -0300

    office

---
+++ Vista/public/misDatos.php
@@ -0,0 +1,9 @@
+<?php
+include_once("../../configuracion.php");
+include_once("../../Utiles/sessmanager.php");
+include_once("../estructura/header.php");
+?>
+<div class="content-wrapper">
+
+</div>
+<?php include_once("../estructura/footer.php"); ?>
