235f912a922070c80084275b4bef3e52d6d417dd
Raam4 <ramiro_cardozo22@hotmail.com>
2021-11-19 14:25:32 -0300

    office

--- Vista/accion/deposito/accionGuardarProd.php
+++ Vista/accion/deposito/accionGuardarProd.php
@@ -11,5 +11,8 @@ if(!isset($data['idproducto'])){
 if($_FILES){
     $ruta = '../../files/prods/';
     $nombre = md5($data['pronombre'].$idprod).'.'.pathinfo($_FILES['imgprod']['name'],PATHINFO_EXTENSION);
+    if(!isset($data['idproducto'])){
+        unlink('../'.$ruta.$nombre);
+    }
     move_uploaded_file($_FILES['imgprod']['tmp_name'], '../'.$ruta.$nombre);
 }
