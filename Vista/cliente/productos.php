c1ec6a721b73364fe909f67ff4b76f1088f78c7e
Lucia-Landaeta <lucia.landaeta@est.fi.uncoma.edu.ar>
2021-11-18 21:35:57 -0300

    .

--- Vista/cliente/productos.php
+++ Vista/cliente/productos.php
@@ -20,7 +20,7 @@ $carroEnCarga = $objSess->getCarrito();
             </div>
             <div class="carousel-inner">
             <div class="carousel-item">
-            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><image width="100%" x="5" y="-15"  href="../css/img/7.jpg" /></svg>
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="xMidYMid slice" focusable="false"><image width="100%"  y="-15"  href="../css/img/7.jpg" /></svg>
                 <div class="container">
                 <div class="carousel-caption text-start">
                     <h1 class="txtArchBl">Salvamos tu previa</h1>                    
@@ -34,8 +34,8 @@ $carroEnCarga = $objSess->getCarrito();
 
                 <div class="container">
                 <div class="carousel-caption text-start">
-                    <h1 class="txtArchBl">Encuantro casual</h1>
-                    <p  class="txtArchBl"><b>Toc Toc te balca</b></p>
+                    <h1 class="txtArchBl">Encuentro casual</h1>
+                    <p  class="txtArchBl"><b>Toc Toc te banca</b></p>
                     <!-- <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p> -->
                 </div>
                 </div>
